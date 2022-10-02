a141beb195b50dc205bc84e60db38778ed48746d
Vilstrups <103036548+Vilstrups@users.noreply.github.com>
2022-10-02 20:56:37 +0200

    Done

---
+++ Assets/RPG Cameras & Controllers/Integration/RPGBuilder/README.txt
@@ -0,0 +1,15 @@
+Integration with RPGBuilder v2.0 by Blink Studios 
+https://assetstore.unity.com/packages/tools/game-toolkits/rpg-builder-177657
+
+0) Import my asset from the Package Manager (Window > Package Manager > My Assets)
+1) Import Unity's new Input System here as well but from the Unity Registry
+2) Activate the new input system by going to Edit > Project Settings > Player > Active Input Handling and selecting "Both" from the drop-down list
+3) Import the files of the provided integration unitypackage (Integration > RPGBuilder) by double-clicking it and pressing the "Import" button
+4) Assign the provided prefab(s) inside the RPG Builder editor at the top (BLINK > RPG Builder) under Character > Races > Genders
+5) Uncheck "Dynamic Animator" and Save
+6) Done
+
+Since it is hard for me to test every single RPGBuilder feature, please report any integration bugs you find via https://johnstairs.com/rcc/#contact or write me in the Blink Discord - thanks!
+
+Best regards,
+John
---
+++ Assets/RPG Cameras & Controllers/Scripts/Water.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace JohnStairs.RCC {
+    public class Water : MonoBehaviour {
+        /// <summary>
+        /// Water height/level in world coordinates, set in Awake
+        /// </summary>
+        protected float _globalWaterHeight = 0;
+
+        /// <summary>
+        /// Comparer class for comparing two waters
+        /// </summary>
+        public class WaterComparer : Comparer<Water> {
+            /// <summary>
+            /// Compares two waters by their water level
+            /// </summary>
+            /// <param name="a">Left-side water script</param>
+            /// <param name="b">Right-side water script</param>
+            /// <returns>A signed number indicating the relative values of a and b</returns>
+            public override int Compare(Water a, Water b) {
+                return a.GetHeight().CompareTo(b.GetHeight());
+            }
+        }
+
+        protected virtual void Awake() {
+            // Set the global water height once
+            _globalWaterHeight = transform.position.y + GetComponent<BoxCollider>().size.y * transform.localScale.y * 0.5f;
+            // Disable the ZWrite property
+            DisableShaderZWrite();
+        }
+
+        /// <summary>
+        /// Disables the ZWrite property of each material of this game object
+        /// </summary>
+        public virtual void DisableShaderZWrite() {
+            Renderer[] renderers = GetComponentsInChildren<Renderer>();
+            foreach (Renderer r in renderers) {
+                Utils.DisableZWrite(r);
+            }
+        }
+
+        /// <summary>
+        /// Gets the water height/level in world coordinates
+        /// </summary>
+        /// <returns>Water level in world coordinates</returns>
+        public virtual float GetHeight() {
+            return _globalWaterHeight;
+        }
+    }
+}
